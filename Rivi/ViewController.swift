73a2071dc06120272a67960467656c56987e4174
amandeep <amandeep@marsplay.co>
2020-01-18 23:47:19 +0530

    Initial Commit

---
+++ Rivi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Rivi
+//
+//  Created by amandeep saluja on 18/01/20.
+//  Copyright © 2020 amandeep saluja. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
